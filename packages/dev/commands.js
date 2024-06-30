1c55c1d5cd1969c5fb95c21d6bcae59721e9c475
saifaldin14 <saifaldindeveloper@gmail.com>
2024-06-29 22:00:47 -0400

    Create commands.js

---
+++ packages/dev/commands.js
@@ -0,0 +1,69 @@
+import chokidar from "chokidar";
+import { spawn } from "node:child_process";
+import initDev from "./init-dev.js";
+import create from "./create.js";
+// `makeDebug` is a function from the "debug" package for creating a debugger instance.
+import makeDebug from "debug";
+
+// Creating a debug instance specifically for "mec:frontend-client-view".
+const debug = makeDebug("mec:commands");
+
+// Variable to store the server process instance
+let server;
+
+/**
+ * Restarts the server process.
+ * If a server process is already running, it is killed with SIGTERM signal.
+ * Then, a new server process is started with the command "node --no-deprecation app.js".
+ * The new server process inherits the standard I/O streams of the parent process.
+ *
+ * @returns {void}
+ */
+const restartServer = () => {
+  if (server) {
+    // If a server process is already running, kill it with SIGTERM signal
+    server.kill("SIGTERM");
+  }
+
+  // Start a new server process
+  server = spawn("node", ["--no-deprecation", "app.js"], { stdio: "inherit" });
+
+  // Add an event listener for when the server process closes
+  server.on("close", (code, signal) => {
+    if (signal) {
+      // If the server process is killed with a signal
+      debug(`Server process was killed with signal ${signal}`);
+    } else if (code !== null) {
+      // If the server process exits with a non-zero code
+      console.log(`Server process exited with code ${code}`);
+    } else {
+      // If the server process exits normally
+      console.log("Server process exited");
+    }
+  });
+}
+
+/**
+ * Restarts the server when a change is detected in the current directory.
+ *
+ * Watches for changes in the current directory and restarts the server
+ * whenever a change is detected. Ignores changes in the 'node_modules' directory
+ * and hidden files/folders.
+ *
+ * @returns {void}
+ */
+const restartDev = () => {
+  const ignores = ["node_modules", /(^|[\/\\])\../];
+
+  // Start watching the current directory for changes
+  chokidar
+    .watch(".", {
+      ignored: ignores,
+    })
+    .on("all", (event, path) => {
+      // When a change is detected, restart the server
+      restartServer();
+    });
+}
+
+export { restartDev, create, initDev };
